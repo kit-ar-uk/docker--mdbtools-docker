d0ca44cef135c5062859f369538bc6ce241c1066
Rainer Rillke <rillke@wikipedia.de>
2018-08-12 17:49:03 +0200

    to_mysql.sh: Do not output tmp tables, shell check

--- scripts/to_mysql.sh
+++ scripts/to_mysql.sh
@@ -4,17 +4,16 @@
 # https://stackoverflow.com/a/32423570
 # Usage: ./to_mysql.sh database.mdb > data.sql
 
-TABLES=$(mdb-tables -1 $1)
+TABLES=$(mdb-tables -1 "$1")
 
 for t in $TABLES
 do
-    echo "DROP TABLE IF EXISTS $t;"
+    [[ "$t" != ~TMP* ]] && \
+	echo "DROP TABLE IF EXISTS $t;" && \
+	mdb-schema "$1" -T "$t" mysql
 done
-
-mdb-schema $1 mysql
 
 for t in $TABLES
 do
-    mdb-export -D '%Y-%m-%d %H:%M:%S' -I mysql $1 $t
+    [[ "$t" != ~TMP* ]] && mdb-export -D '%Y-%m-%d %H:%M:%S' -I mysql "$1" "$t"
 done
-
